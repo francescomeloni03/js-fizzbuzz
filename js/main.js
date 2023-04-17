008147547fd78014524a5482be3f1793a15a0df9
Francesco Meloni <francesco.meloni03@icloud.com>
2023-04-17 18:30:06 +0200

    parte 2

--- js/main.js
+++ js/main.js
@@ -1,13 +1,49 @@
 
 
 const contentitore = document.getElementById("container");
-const ul = document.querySelector("ul.list");
 
-for (let contatore = 1; contatore < 101; contatore++) {
+for (let contatore = 0; contatore < 100; contatore++) {
 
     console.log(contatore);
+
+    let ilNostroIndice = contatore + 1;
+
+    let numero = ilNostroIndice ;
+
+
+    
+   
+
+    if (numero % 3 == 0){
+        contentitore.innerHTML += `<div class="box-fizz">fizz</div>`;
+    }
+
+    else if (numero % 5 == 0){
+        contentitore.innerHTML += `<div class="box-buzz">buzz</div>`;
+    }
+
+    else if (numero % 3 == 0 && numero % 5== 0){
+        contentitore.innerHTML += `<div class="box-fizz-buzz">fizzbuzz</div>`;  //multipli sia di 3 che di 5 non mi esce, domani vedo //
+    }
+
+    else {
+        contentitore.innerHTML += `<div class="box">${numero}</div>`;
+    }
+
+ 
+
+
     
 }
+
+
+
+
+
+
+
+
+
 
 
 
