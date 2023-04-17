915890533648663bedc07edba4e5a87e3757edec
Francesco Meloni <francesco.meloni03@icloud.com>
2023-04-17 17:07:24 +0200

    Parte da 1 a 100

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+const contentitore = document.getElementById("container");
+const ul = document.querySelector("ul.list");
+
+for (let contatore = 1; contatore < 101; contatore++) {
+
+    console.log(contatore);
+    
+}
+
+
+
