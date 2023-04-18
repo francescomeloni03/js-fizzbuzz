071d5c36f75778823a4c68d89c6c4f7b26530d60
Francesco Meloni <francesco.meloni03@icloud.com>
2023-04-18 10:33:38 +0200

    Stampo in console

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const contentitore = document.getElementById("container");
 
 for (let contatore = 0; contatore < 100; contatore++) {
 
-    console.log(contatore);
+    // console.log(contatore);
 
     let ilNostroIndice = contatore + 1;
 
@@ -14,18 +14,22 @@ for (let contatore = 0; contatore < 100; contatore++) {
     
     if (numero % 3 == 0 && numero % 5== 0){
         contentitore.innerHTML += `<div class="box-fizz-buzz">fizzbuzz</div>`;   //Ora funziona: ho cambiato ordine
+        console.log("fizzbuzz");
     }
 
     else if (numero % 3 == 0){
         contentitore.innerHTML += `<div class="box-fizz">fizz</div>`;
+        console.log("fizz");
     }
 
     else if (numero % 5 == 0){
         contentitore.innerHTML += `<div class="box-buzz">buzz</div>`;
+        console.log("buzz");
     }
 
     else {
         contentitore.innerHTML += `<div class="box">${numero}</div>`;
+        console.log(numero);
     }
 
  
